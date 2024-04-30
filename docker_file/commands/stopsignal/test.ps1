7b8e8911c2b8c9e6357f95587a688378540ddca8
spech13 <es-popov2@yandex.ru>
2024-05-01 00:11:25 +0300

    Refactoring test for STOPSIGNAL instruction

--- docker_file/commands/stopsignal/test.ps1
+++ docker_file/commands/stopsignal/test.ps1
@@ -1,19 +1,18 @@
 docker build -qt stopsignal .
-
 docker run -d --name stopsignal-container stopsignal
-
 docker stop stopsignal-container
 
 $exit_code = (docker inspect --format="{{.State.ExitCode}}" stopsignal-container)
 
 docker rm stopsignal-container
-
 docker rmi stopsignal
 
-clear
+Clear-Host
 
 if($exit_code -eq 137){
+    Write-Host "---------------------------------------------"
+    Write-Host "SUCCESS TEST"
     write-host "Exit code cotainer: SIGKILL == $exit_code"
 }else{
-    write-host "Error test"
+    write-host "ERROR TEST"
 }
