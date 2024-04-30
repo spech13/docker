5ca86d1fe031339b95f1562c475720f64285a5f5
spech13 <es-popov2@yandex.ru>
2024-04-30 16:15:19 +0300

    Refactoring test for MAINTAINER instruction

--- docker_file/commands/maintainer/test.ps1
+++ docker_file/commands/maintainer/test.ps1
@@ -1,9 +1,15 @@
 docker build -qt test-image .
 
-$author = docker inspect --format="{{.Author}}" test-image
-
-write-host "----------------------------------------------"
-write-host "Image author: $author"
-write-host "----------------------------------------------"
+$author_name = (docker inspect --format="{{json .Author}}" test-image) | convertfrom-json
 
 docker rmi test-image
+
+clear
+
+if($author_name -eq "author-name"){
+    write-host "-----------------------------------------"
+    write-host "SUCCESS"
+    write-host "Author name: $author_name"
+}else{
+    write-host "ERROR TEST"
+}
